c2ee6d6fce5258093e1159a694296df01527b09a
casey962 <cadraper@my.waketech.edu>
2025-04-11 11:34:19 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,16 @@
+// todo: fetch data from the postgreSQL database (to be implemented later)
+function fetchGradeData() {
+  // This function will query the postegreSQL database and return grade data 
+  console.log("Fetching grade data...");
+}
+
+//Todo: Populate the table with grade data
+function populateGradebook(data) {
+  // This function will take the fetched grade dat and populate the table
+  console.log("populating gradebook with data:", data);
+}
+// Todo REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populategradebook(gradeData);
+// END REMOVE
